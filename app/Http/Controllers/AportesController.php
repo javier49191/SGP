e14bae18b7e00d99599da526fb41914eec8b7a74
javier49191 <javier49191@gmail.com>
2019-07-04 10:24:31 -0300

    Cambios en el login

--- app/Http/Controllers/AportesController.php
+++ app/Http/Controllers/AportesController.php
@@ -15,6 +15,15 @@ class AportesController extends Controller
         Carbon::setLocale('es');
     }
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
