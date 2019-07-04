70df426984bb48e56fa06431cc2edf8168ad71d0
javier49191 <javier49191@gmail.com>
2019-07-04 16:37:06 -0300

    Cambios en las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('alumnos', 'AlumnosController');
 Route::resource('padrinos', 'PadrinosController');
