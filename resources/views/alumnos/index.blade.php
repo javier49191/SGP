5c2d9a03a7e89046e11d8d9b045d4118ab87fea7
javier49191 <javier49191@gmail.com>
2019-07-07 14:48:06 -0300

    Cambios en los cdn

--- resources/views/alumnos/index.blade.php
+++ resources/views/alumnos/index.blade.php
@@ -81,7 +81,7 @@
 	@endif
 </script>
 
-<script src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 <script>
 	$(document).ready( function () {
